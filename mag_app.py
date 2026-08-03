f5bd7fdb3524f059be92b001f771c2fe4da71a5a
kowelt1569 <kowelt1569@gmail.com>
2026-08-03 18:15:42 +0300

    Update mag_app.py

--- mag_app.py
+++ mag_app.py
@@ -191,7 +191,7 @@ with st.sidebar:
         st.session_state.coeffs           = c
         st.session_state.metrics["R2"]    = f"{r2:.4f}"
         st.session_state.metrics["RMSE"]  = f"{rmse:.4f}"
-                st.success("Модель успішно навчено!")
+        st.success("Модель успішно навчено!")
                 st.rerun()
             except ValueError:
                 st.error("Помилка даних: таблиця містить нечислові або порожні значення!")
